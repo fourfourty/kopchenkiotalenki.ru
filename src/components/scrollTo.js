cee0eb1062ee3c11566723ca1a17bb7b8705e3a3
Artem Dybin <tema.dybin@yandex.ru>
2021-04-06 21:15:07 +0300

    Delete src directory

--- src/components/scrollTo.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-function scrollTo( scroll ){
-
-    $('html,body').stop().animate({ scrollTop: $(`[name=`+ scroll + `]`).offset().top }, 1500);
-};
-
-export default scrollTo;
